01b72a5d8f950d524cf6e72822a355fcc175f3e3
KafuiPraise <matildapraise@gmail.com>
2023-04-27 15:35:21 -0700

    ur message

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+/**
+ * main - Test function for positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
 
-#include <stdio.h>
+	i = 0;
 
-void positive_or_negative(int i);
+	positive_or_negative(i);
+	return (0);
+}
 
-#endif
