ca504d028e4f42592a625b4ba05decf10f97bb92
KafuiPraise <matildapraise@gmail.com>
2023-05-08 20:33:03 -0700

    even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _strspn - Entry point
+ *  _strspn - Entry point
  * @s: input
  * @accept: input
- * Return: Always 0 (Success)
- */
+ * Return: Always 0
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int n = 0;
+	unsigned int a = 0;
 	int r;
 
 	while (*s)
@@ -16,13 +16,13 @@ unsigned int _strspn(char *s, char *accept)
 		{
 			if (*s == accept[r])
 			{
-				n++;
+				a++;
 				break;
 			}
 			else if (accept[r + 1] == '\0')
-				return (n);
+				return (a);
 		}
 		s++;
 	}
-	return (n);
+	return (a);
 }
