faf9977710fca50e7e537fa570c37810cee27b2e
KafuiPraise <matildapraise@gmail.com>
2023-05-22 13:37:16 -0700

    Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's info
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ *
+ * Description: more detailed description
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
