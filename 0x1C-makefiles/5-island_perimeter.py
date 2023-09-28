ed1b4c3f475386185647783f15cb1de364a6d911
kafuipraise <matildapraise@gmail.com>
2023-09-27 18:08:57 -0700

    All Task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""Calculates an island perimeter measuring function."""
+"""interview prep task: Island Perimeter"""
 
 
 def island_perimeter(grid):
-   """Returns the number of water neighbors a cell in grid."""
-
+    """Returns perimiter of an island"""
+    shape = 0
+    sizes = 0
+    length = len(grid)
     width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
-
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
-    return size * 4 - edges * 2
+    for x in range(length):
+        for y in range(width):
+            if grid[x][y] == 1:
+                sizes = sizes + 1
+                if (y > 0 and grid[x][y - 1] == 1):
+                    shape += 1
+                if (x > 0 and grid[x - 1][y] == 1):
+                    shape += 1
+    return sizes * 4 - shape * 2
